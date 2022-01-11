f763ecc46eee741a038c10e0913fa2e7504ac539
jsliyuan <jsliyuan@gmail.com>
2022-01-11 19:56:56 +0800

    Tests for the 2 new functions
    print_anf
    get_degree

--- test.cpp
+++ test.cpp
@@ -7,10 +7,10 @@ using namespace std;
 int main() {
   BooleanFun f(6);
   assert(f.var_num() == 6);
-
   assert(f.value(4, 0,0,0,0) == -1);
-
   assert(f.value(6, 0,0,1,1,1,1) == 0);
+  assert(f.get_anf() == "0");
+  assert(f.get_degree() == 0);
   cout << "End of test for f." << endl;
 
 
@@ -18,12 +18,16 @@ int main() {
   assert(g.var_num() == 5);
   assert(g.value(5, 0, 0, 0, 0, 0) == 1);
   assert(g.value(5, 0, 0, 1, 1, 0) == 1);
+  assert(g.get_anf() == "1");
+  assert(g.get_degree() == 0);
   cout << "End of test for g." << endl;
 
   BooleanFun g2(5, "1+1+0");
   assert(g2.var_num() == 5);
   assert(g2.value(5, 0, 0, 0, 0, 0) == 0);
   assert(g2.value(5, 0, 0, 1, 1, 0) == 0);
+  assert(g2.get_anf() == "0");
+  assert(g2.get_degree() == 0);
   cout << "End of test for g2." << endl;
 
   BooleanFun g3_xor(3, "x1+x2+x3");
@@ -32,6 +36,8 @@ int main() {
   assert(g3_xor.value(3, 1, 1, 0) == 0);
   assert(g3_xor.value(3, 1, 1, 1) == 1);
   assert(g3_xor.value(3, 0, 0, 1) == 1);
+  assert(g3_xor.get_anf() == "x3+x2+x1");
+  assert(g3_xor.get_degree() == 1);
   cout << "End of test for g3_xor." << endl;
 
   BooleanFun g3_and(3, "x1x2x3");
@@ -39,6 +45,8 @@ int main() {
   assert(g3_and.value(3, 0, 0, 0) == 0);
   assert(g3_and.value(3, 1, 1, 0) == 0);
   assert(g3_and.value(3, 1, 1, 1) == 1);
+  assert(g3_and.get_anf() == "x1x2x3");
+  assert(g3_and.get_degree() == 3);
   cout << "End of test for g3_and." << endl;
 
   BooleanFun g4(4, "x1+1");
@@ -47,6 +55,8 @@ int main() {
   assert(g4.value(4, 1, 0, 1, 0) == 0);
   assert(g4.value(4, 1, 0, 0, 0) == 0);
   assert(g4.value(4, 0, 1, 1, 1) == 1);
+  assert(g4.get_anf() == "1+x1");
+  assert(g4.get_degree() == 1);
   cout << "End of test for g4." << endl;
 
   BooleanFun g5(5, "x1x2x3+x4x5");
@@ -55,6 +65,8 @@ int main() {
   assert(g5.value(5, 1, 1, 1, 1, 1) == 0);
   assert(g5.value(5, 0, 1, 0, 1, 1) == 1);
   assert(g5.value(5, 0, 0, 0, 0, 1) == 0);
+  assert(g5.get_anf() == "x4x5+x1x2x3");
+  assert(g5.get_degree() == 3);
   cout << "End of test for g5." << endl;
 
 
