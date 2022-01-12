7c1a72536b4fda84cc3a353089284917c33d9a67
jsliyuan <jsliyuan@gmail.com>
2022-01-12 15:56:03 +0800

    Add tests for negate, add, mult

--- test.cpp
+++ test.cpp
@@ -84,6 +84,46 @@ int main() {
   assert(g3_and2.is_equal(g3_and));
   cout << "End of test for g3_and2." << endl;
 
+  // Be careful. g3_and is changed!
+  assert(g3_and.add(g3_xor) == true);
+  assert(g3_and.get_anf() == "x3+x2+x1+x1x2x3");
+  cout << "End of test for g3_and + g3_xor" << endl;
+
+  BooleanFun g3_zero(3, "0");
+  // Be careful, g3_and is set to zero.
+  assert(g3_and.add(g3_and) == true);
+  assert(g3_and.is_equal(g3_zero));
+  cout << "End of test for g3_and + g3_and = 0" << endl;
+
+  assert(g3_and.add(g5) == false);
+  cout << "End of test for g3_and + g5" << endl;
+
+  BooleanFun g3_one(3, "1");
+  assert(g3_and2.mult(g5) == false);
+  assert(g3_and2.mult(g3_one) == true);
+  assert(g3_and2.get_anf() == "x1x2x3");
+  // (x1+x2+x3)*x1x2x3 = x1x2x3
+  assert(g3_and2.mult(g3_xor) == true);
+  assert(g3_and2.get_anf() == "x1x2x3");
+
+  BooleanFun g5_d2(5, "x1x2+x3x5");
+  // (x1x2 + x3x5) * (x1x2x3+x4x5)
+  // = x1x2x3 + x1x2x4x5 + x1x2x3x5 + x3x4x5
+  assert(g5_d2.mult(g5) == true);
+  assert(g5_d2.get_anf() == "x3x4x5+x1x2x4x5+x1x2x3+x1x2x3x5");
+  assert(g5_d2.get_degree() == 4);
+  cout << "End of test for mult()" << endl;
+
+  g3_one.negate();
+  assert(g3_one.get_anf() == "0");
+  g3_one.negate();
+  assert(g3_one.get_anf() == "1");
+  cout << "End of test for negate()" << endl;
+  g3_xor.negate();
+  assert(g3_xor.get_anf() == "1+x3+x2+x1");
+  g3_xor.negate();
+  assert(g3_xor.get_anf() == "x3+x2+x1");
+
   cout << "Everything looks good. End of all tests." << endl;
   return 0;
 }
