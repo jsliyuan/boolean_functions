b93f0e3523a36f4a7ba7332765fbe2f1cb6d8d2b
jsliyuan <jsliyuan@gmail.com>
2022-01-14 09:47:54 +0800

    Update example64.cpp
    minor change

--- example64.cpp
+++ example64.cpp
@@ -11,11 +11,15 @@ int main() {
   UpperDiagGenerator gen6(6);
   int total = 0;
   set<string> all_bfs;
+  BooleanFun f64(6);
+
   while (1) {
-    BooleanFun f64(6, "x1x2x3x6+x1x2x4x5");
+    f64.set_anf("x1x2x3x6+x1x2x4x5");
     f64.apply_affine_trans(*gen6.get_affine_trans());
+    // Trim all monomials with degree <= 3.
     f64.trim_degree_below(4);
     string anf = f64.get_anf();
+    // Make sure it hasn't been added.
     if (all_bfs.find(anf) == all_bfs.end()) {
       all_bfs.insert(anf);
       cout << anf << endl;
