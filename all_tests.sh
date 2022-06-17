3576d511466724bcecb206d4d7fdcae1b1e64bd0
jjgaoRita <97389626+jjgaoRita@users.noreply.github.com>
2022-06-17 16:20:49 +0800

    Update all_tests.sh

--- all_tests.sh
+++ all_tests.sh
@@ -24,3 +24,7 @@ g++ -o test.out test_nonsingular_generator.cpp nonsingular_generator.cpp affine_
 echo "test HomogenousBfGenerator class"
 g++ -o test.out test_homogenous_bf_generator.cpp homogenous_bf_generator.cpp boolean_fun.cpp affine_trans.cpp
 ./test.out
+
+echo "test Permutation class"
+g++ -o test.out test_permutation.cpp permutation.cpp
+./test.out
