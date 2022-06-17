dab56df641a34d4ce16987be2cda06aac44649d5
jjgaoRita <97389626+jjgaoRita@users.noreply.github.com>
2022-06-17 16:21:44 +0800

    Update permutation.h

--- permutation.h
+++ permutation.h
@@ -30,25 +30,33 @@ class Permutation {
     bool set(int idx, int val);
     
     // Return "2,3,4,5,6,7,8,9,1"
-    std::string get_str();
+    std::string get_str() const;
 
     const int* get_perm() {
-      return this->perm;
+      return perm;
     }
 
     // Assignment operator
     Permutation& operator=(const Permutation& perm1);
     
+    // Reload = opeartor
+    bool operator == (const Permutation &p) const {
+      if (this->n != p.var_num()) {
+        return false;
+      }
+      return this->get_str() == p.get_str();
+    }
+
     // this = perm * this
     void left_mult(const Permutation& perm1);
     
     // this = this * perm
     void right_mult(const Permutation& perm1);
-  
+
   private:
     int n;
 
-    // array of length n  
+    // array of length n
     int* perm;
 
     void copy_data(const Permutation& perm1);
