1009c3559f0228822160352fe248442b70b01382
jjgaoRita <97389626+jjgaoRita@users.noreply.github.com>
2022-06-18 18:14:00 +0800

    Update all_tests.sh

--- all_tests.sh
+++ all_tests.sh
@@ -28,5 +28,5 @@ g++ -o test.out test_permutation.cpp permutation.cpp
 ./test.out
 
 echo "test Rotationsym class"
-g++ -o test.out test_rotationsym.cpp rotationsym.cpp permutation.cpp
+g++ -o test.out test_rotation_sym.cpp rotation_sym.cpp permutation.cpp
 ./test.out
