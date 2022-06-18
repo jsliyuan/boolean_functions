0bcc6ed058d27454ab37b06df324048f400a0749
jjgaoRita <97389626+jjgaoRita@users.noreply.github.com>
2022-06-18 15:45:44 +0800

    Update rotationsym.h

--- rotationsym.h
+++ rotationsym.h
@@ -5,18 +5,19 @@
 #include <unordered_set>
 #include <vector>
 #include "permutation.h"
+
 using namespace std;
-// Affine transformation Ax + b: GF(2)^n -> GF(2)^n,
-// where A is an n*n matrix, and b is an n*1 array.
+
 class RotationSym {
   public:
-    // Constructor with parameter (dimension).
-    // Create an all-zero affine transformation.
+    // Initialize Rotationsym
+    // generate all permutations of rotation symmetric class and orbits
     RotationSym(int n);
 
     // Copy constructor.
     // Performs a deep copy.
     RotationSym(const RotationSym& RS);
+  
     // Destructor.
     ~RotationSym();
 
@@ -33,25 +34,27 @@ class RotationSym {
     int get_orbits_number() const;
 
     // x is between [1,2^n]
-    // return the orbit of x
+    // return the orbit of the position x
     std::vector<int> get_orbit(int x);
 
     // return the ith orbit  
     std::vector<int> get_full_orbit(int i) const;
 
     int get_n() const;
+
+    std::vector< std::vector<int> > get_all_orbits();
     
   private:
-    // dimension
+    // number of variables
     int n;
     
     void copy_data(const RotationSym& G);
-
+    
+    //store permutations
     std::vector<Permutation> perms;
 
+    //store all orbits 
     std::vector<vector<int> > orbits;
 };
 
 #endif
-
-
