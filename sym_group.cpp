f0ba7c4075f1f7c2ced6ae89a5f94827ed525ade
jjgaoRita <97389626+jjgaoRita@users.noreply.github.com>
2022-06-30 08:20:15 +0800

    Add files via upload

---
+++ sym_group.cpp
@@ -0,0 +1,209 @@
+#include "sym_group.h"
+#include "permutation.h"
+
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <vector>
+#include <set>
+#include <sstream>
+#include <iterator>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+SymGroup::SymGroup(int n) {
+  this->n = n;
+  this->generators.clear();
+  this->elements.clear();
+  //initialize permuration 
+  Permutation *identity= new Permutation(n);
+  for(int i=1; i <= n ; i++) {
+    (*identity).set(i,i);
+  }
+  elements.push_back(*identity);
+
+  //initialize random seed
+  srand(time(NULL));
+}
+
+// copy all data from g to this.
+void SymGroup::copy_data(const SymGroup& SG) {
+  this->n = SG.get_n();
+  this->generators.clear();
+  this->elements.clear();
+  this->orbits.clear();
+  for (const Permutation &it: SG.generators) {
+    this->generators.push_back(it);
+  }
+  for (const Permutation &itt: SG.elements) {
+    this->elements.push_back(itt);
+  }
+  for (const vector<int> &iter: SG.orbits) {
+    this->orbits.push_back(iter);
+  }
+}
+
+// Copy constructor.
+SymGroup::SymGroup(const SymGroup& SG) {
+  copy_data(SG);
+}
+
+// Assignment operator
+SymGroup& SymGroup::operator=(const SymGroup& SG) {
+  copy_data(SG);
+  return *this;
+}
+
+void SymGroup::generate_all() {
+  for (vector<Permutation>::iterator it= this->generators.begin();it!=this->generators.end();it++) {
+    elements.push_back(*it);
+  }
+  vector<Permutation> queue;
+  queue=this->elements;
+  int head=0,tail=queue.size()-1; 
+  while(head<=tail) {
+    Permutation *cur=new Permutation(n);
+    *cur=queue.at(head);
+    vector<Permutation> term;
+    term=this->elements;
+    for( vector<Permutation>::iterator it= term.begin();it!=term.end();it++) {
+      Permutation *new_element=new Permutation(n);
+      *new_element=*cur;
+      (*new_element).left_mult(*it);
+      vector<Permutation>::iterator iter;
+      iter=find(this->elements.begin(),this->elements.end(),*new_element);
+      if(iter==this->elements.end()) {
+        this->elements.push_back(*new_element);
+        queue.push_back(*new_element);
+        tail++;
+      }
+    } 
+    head++;
+  }  
+}
+
+void SymGroup::compute_orbits() {
+  generate_all();
+  int count=0;
+  for (int x=0;x<(1<<n);x++) {
+    vector<int> bin;
+    int num1;
+
+    // transform decimal to binary
+    int number=x;
+    for (int t =n-1; t>-1; t--) {
+      num1= (number>>t) ;
+      number =number- num1*(pow(2,t));
+      bin.push_back(num1);
+    }
+
+    vector <int>::iterator iter1;
+    int flag=0;
+
+    // whether the orbit of x has been found
+    for(vector<vector<int> >::iterator it= this->orbits.begin();it!=this->orbits.end();it++) {
+      iter1=find((*it).begin(),(*it).end(),x);
+      if(iter1!=(*it).end()) {
+        flag=1;
+        break;
+      } 
+    }
+    
+    // if the orbit of x has not been found
+    if(flag==0) {
+      count++;
+      int value=0;
+      vector<int> bin1;
+      
+      for (vector<Permutation>:: iterator iter=this->elements.begin();iter!=this->elements.end();iter++) {
+        const int* term=(*iter).get_perm();
+
+        // permutation of x
+        int* store;
+        store=new int[n];
+        for (int j=0;j<n;j++) {
+          int pos=term[j]-1;
+          store[j]=bin[pos];
+        }
+        //binary to decimal
+        for(int p=0;p<n;p++) {
+          value=value+(store[p]<<(n-1-p));
+        }
+        vector <int>::iterator itt;
+        itt=find(bin1.begin(),bin1.end(),value);
+        if (itt == bin1.end()) {
+          // vector b is the orbit of i
+          bin1.push_back(value);
+        }
+        value=0;
+      }
+      orbits.push_back(bin1);
+    }
+  }
+}
+
+// Returns the orbit of x .
+std::vector<int> SymGroup::get_orbit(int x) {
+  vector<int> cin;
+  vector<int> din;
+  int num;
+  int value=0;
+  // decimal to binary
+  int number=x;
+  for (int t =n-1; t>-1; t--) {
+    num= (number>>t) ;
+    number=number-num*(pow(2,t));
+    cin.push_back(num);
+  }
+  for (vector<Permutation>:: iterator iter= this->elements.begin();iter!=this->elements.end();iter++) {
+    const int* term=(*iter).get_perm();
+    
+    // permutation of x
+    int* store;
+    store=new int[n];
+    for (int j=0;j<n;j++) {
+      int pos=term[j]-1;
+      store[j]=cin[pos];
+    }
+    //binary to decimal after permutation
+    for(int j=0;j<n;j++) {
+      value=value+(store[j]<<(n-1-j));
+    }
+
+    vector <int>::iterator itt;
+    itt=find(din.begin(),din.end(),value);
+    if (itt == din.end()) {
+      // vector d is the orbit of i
+      din.push_back(value);
+    }
+    value=0;
+  }
+  
+  return din;
+}
+
+std::vector<int> SymGroup::get_full_orbit(int i) const {
+  return this->orbits.at(i);
+}
+
+std::vector< std::vector<int> > SymGroup::get_all_orbits() {
+  return this->orbits;
+}
+
+int SymGroup::get_orbits_number() const {
+  return this->orbits.size();
+}
+
+int SymGroup::get_n() const {
+  return this->n;
+}
+
+SymGroup::~SymGroup()
+{
+  this->generators.clear();
+  this->elements.clear();
+  this->orbits.clear();
+}
---
+++ sym_group.h
@@ -0,0 +1,86 @@
+#ifndef SYM_GROUP_H
+#define SYM_GROUP_H
+
+#include <string>
+#include <unordered_set>
+#include <vector>
+#include <set>
+#include "permutation.h"
+using namespace std;
+
+class SymGroup {
+  public:
+    
+    SymGroup(int n);
+
+    SymGroup(const SymGroup& SG);
+    // Destructor.
+    ~SymGroup();
+
+    // Assignment operator
+    SymGroup& operator=(const SymGroup& SG);
+    
+    bool operator == (const SymGroup &SG) const {
+      if (this->n != this->n) {
+        return false;
+      }
+    //  return this->get_str() == p.get_str();
+    }
+
+   
+    void set_generator(Permutation perm) {
+        generators.push_back(perm);
+    }
+    // Can be called multiple times.
+    void add_generator(Permutation perm1) {
+      this->generators.push_back(perm1);
+    }
+
+    void compute_orbits();
+
+    vector<Permutation> get_elements() {
+      return this->elements;
+    }
+
+    vector<Permutation> get_generators() {
+      return this->generators;
+    }
+
+    int get_orbits_number() const;
+
+    // x is between [1,2^n]
+    // return the orbit of x
+    std::vector<int> get_orbit(int x);
+
+    // return the i-1 th orbit  
+    std::vector<int> get_full_orbit(int i) const;
+
+    int get_n() const;
+
+    int group_size() const {
+      return this->elements.size();
+    }
+
+    std::vector< std::vector<int> > get_all_orbits();
+    
+  private:
+    // dimension
+    int n;
+    
+    void copy_data(const SymGroup& SG);
+
+    vector<Permutation> generators;
+
+     // All elements of the group
+    vector<Permutation> elements;
+
+    // Given generators, compute all elements of the group.
+    void generate_all();
+
+    vector<vector<int> > orbits;
+
+};
+
+#endif
+
+
