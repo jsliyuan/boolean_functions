bb225c5ee002f61932504b1e35d5a5212c337b51
jjgaoRita <97389626+jjgaoRita@users.noreply.github.com>
2022-11-29 14:22:20 +0800

    Update test_nl2_for_8v.cpp

--- examples/test_nl2_for_8v.cpp
+++ examples/test_nl2_for_8v.cpp
@@ -33,22 +33,7 @@ int main() {
   BooleanFunDecoder f1(8);
   // You can replace the string here with whatever the hexadecimal truth table of 8-variable function which you want to test.
   f1.set_truth_table_hex("964841A34106991D4205037895B424C6601B1255213C5C8287659C423942D26A");
-  f1.set_truth_table_done();
-  BooleanFun sub1(7);
-  sub1=f1.sub_function(0);
-  cout<<sub1.nonlinearity(2)<<endl;
-  int max_val=0;
-  HomogenousBfGenerator gen(7,2);
-  while(gen.next()){
-    BooleanFun g(7);
-    g=*gen.get_boolean_fun();
-    g.add(sub1);
-    if(g.nonlinearity()>max_val){
-      max_val=g.nonlinearity();
-    }
-  }
-  cout<<"the max of sub1 is "<<max_val<<endl;
-  
+  f1.set_truth_table_done(); 
   // call second_order_nonlinearity_Fourquet_Tavernier() to compute second-order nonlinearity
   cout<<"The second-order nonlinearity of f1 is "<<f1.second_order_nonlinearity_Fourquet_Tavernier()<<endl;
   
